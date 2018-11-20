e99631adfd98ff86e72f4a4bf891c0c99c080206
yuting7 <yuting7@hawaii.edu>
2018-11-20 10:32:54 -1000

    commit admin home page

--- app/imports/ui/pages/AdminHomePage.jsx
+++ app/imports/ui/pages/AdminHomePage.jsx
@@ -6,8 +6,9 @@ class AdminHomePage extends React.Component {
   render() {
     return (
         <Container textAlign='center'>
-          <Header as='h1' inverted>List of Vendors</Header>
-          <Grid columns={2} divided>
+          <Grid columns={3} divided>
+            <br/>
+            <Header as='h1' inverted>List of Vendors</Header>
             <Grid.Row>
             <Grid.Column>
               <Card>
@@ -31,7 +32,7 @@ class AdminHomePage extends React.Component {
               <Card>
                 <Card.Content>
                   <Card.Header>Holoholo Grill</Card.Header>
-                  <Image src="/images/Honolulu-Grill.jpg"/>
+                  <Image size= "small" src="/images/Honolulu-Grill.jpg"/>
                   <Card.Meta>
                     Paradise Palms Café
                   </Card.Meta>
@@ -43,13 +44,13 @@ class AdminHomePage extends React.Component {
                   </Card.Description>
                 </Card.Content>
               </Card>
-            </Grid.Column>
+            </Grid.Column><br/>
 
             <Grid.Column>
               <Card>
                 <Card.Content>
                   <Card.Header>Le Crêpe Café</Card.Header>
-                  <Image src="/images/Le-Crêpe-Café.jpg"/>
+                  <Image size="tiny" src="/images/Le-Crêpe-Café.jpg"/>
                   <Card.Meta>
                     Richardson School of Law & Paradise Palms Café (2 locations)
                   </Card.Meta>
@@ -57,22 +58,11 @@ class AdminHomePage extends React.Component {
                     Le Crêpe Café makes traditional French crêpes, cooked to order in front of customers. They
                     specialize in a flavorsome menu of sweet and savory crêpes and use fresh ingredients, organic when
                     possible. Their utensils are even bio-compostable. Enjoy a crêpe any time of day, as a breakfast,
-                    snack, main course, or dessert - close your eyes and you're in Paris!<br/>
-                    Whether you’re needing a break from the law books or a pick-me-up as you’re headed to or off campus,
-                    you’ll also appreciate Le Crêpe Café’s French Roast and espresso drinks.
+                    snack, main course, or dessert - close your eyes and you're in Paris!
                   </Card.Description>
                 </Card.Content>
               </Card>
             </Grid.Column>
-            </Grid.Row>
-          </Grid>
-
-          <Header as='h1' inverted>List of Users</Header>
-          <Grid columns={3} divided>
-            <Grid.Row>
-              <Grid.Column></Grid.Column>
-              <Grid.Column></Grid.Column>
-              <Grid.Column></Grid.Column>
             </Grid.Row>
           </Grid>
         </Container>
