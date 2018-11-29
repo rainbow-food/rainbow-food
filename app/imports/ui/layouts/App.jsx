9d953427e2cdee550e399893f6c03d971623ab11
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2018-11-29 09:00:19 -1000

    edit

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,12 +7,17 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
+import ListStuff from '../pages/ListStuff';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
+import AdminHomePage from '../pages/AdminHomePage';
+import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import FoodAvailable from '../pages/FoodsAvailable';
+import TopPicks from '../pages/TopPicks';
 import Profile from '../pages/Profile';
 import SavedVendors from '../pages/SavedVendors';
 
